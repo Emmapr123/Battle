8bb74c8bbe96b668f1d96b8ce276cdb54c82bef3
Emmapr123 <emmapriester@hotmail.com>
2021-03-15 17:52:14 +0000

    added a submit form, that asks for a name. The cat only tells you his name is it is given

--- app.rb
+++ app.rb
@@ -22,6 +22,15 @@ get '/bloop' do
   "bloopdywoop"
 end 
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
+  @age = params[:age]
+  p params[:name]
+  p params[:age]
   erb(:index)
 end
