e530027edf793b5b846af1cfdab08899ca4360b8
Emmapr123 <emmapriester@hotmail.com>
2021-03-15 16:43:55 +0000

    got shotgun to work, ready to shotgun our laptops out the window

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+set :session_secret, 'super secret'
+
+get "/" do
+  "Hello World"
+end 
+
+get '/secret' do
+  "foodbar"
+end
+
+get '/taran' do
+  "Hi Taran"
+end
+
+get '/emma' do
+  "Hi Emma"
+end 
+
+get '/bloop' do
+  "bloopdywoop"
+end 
+
